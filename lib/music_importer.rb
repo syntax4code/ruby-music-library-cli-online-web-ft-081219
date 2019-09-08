5cedd1075816f911e7847d1e16d1865b9484317e
Warren Harper II <warrenharper2@att.net>
2019-09-08 15:24:30 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -2,7 +2,7 @@ class MusicImporter
   attr_reader :path
 
   def initialize(path)# accepts a file path to parse MP3 files from #path
-    @path = path
+    @path = path #path retrieves the path provided to the MusicImporter object
   end
 
   def files
